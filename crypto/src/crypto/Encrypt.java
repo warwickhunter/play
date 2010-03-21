13893685a16502bbf4017bd05e0346194b1f5d91
Warwick Hunter <w.hunter@computer.org>
2010-03-21 18:14:14 +1000

    Not quite working yet

--- crypto/src/crypto/Encrypt.java
+++ crypto/src/crypto/Encrypt.java
@@ -1,10 +1,8 @@
 package crypto;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.AlgorithmParameters;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyStore;
@@ -15,9 +13,11 @@ import java.security.cert.CertificateException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.NullCipher;
 
 /**
  * Java Cryptography experiment. Encrypt a file.
@@ -52,11 +52,15 @@ public class Encrypt
             KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
             FileInputStream keyStoreIn = new FileInputStream(KEYSTORE);
             keyStore.load(keyStoreIn, KEYSTORE_PASSWORD.toCharArray());
-            Key key = keyStore.getKey(KEY_ALIAS, KEY_PASSWORD.toCharArray());
+            keyStoreIn.close();
+            
+            Key    key    = keyStore.getKey(KEY_ALIAS, KEY_PASSWORD.toCharArray());
+            Cipher cipher = Cipher.getInstance("RSA"); // RSA
             
             for (int i = 0; i < args.length; ++i)
             {
-                encrypt(key, args[i]);
+                encrypt(cipher, key, args[i], args[i] + ".crypt");
+                decrypt(cipher, key, args[i] + ".crypt", args[i] + ".decrypt");
             }
         }
         catch (Exception e)
@@ -65,30 +69,47 @@ public class Encrypt
         }
     }
 
-    private static void encrypt(Key key, String filename) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException
+    private static void encrypt(Cipher cipher, Key key, String sourceFile, String destinationFile) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException
     {
-        // get cipher object for password-based encryption
-        Cipher cipher = Cipher.getInstance("PBEWithMD5AndDES");
-        
-        // initialize cipher for encryption, without supplying
-        // any parameters. Here, "myKey" is assumed to refer 
-        // to an already-generated key.
+        System.out.printf("encrypt %s to %s %n", sourceFile, destinationFile);
         cipher.init(Cipher.ENCRYPT_MODE, key);
-        
-        FileInputStream fileIn = new FileInputStream(filename);
-        FileOutputStream fileOut = new FileOutputStream(filename + ".enc");
+        FileInputStream fileIn = new FileInputStream(sourceFile);
+        FileOutputStream fileOut = new FileOutputStream(destinationFile);
         CipherOutputStream cipherOut = new CipherOutputStream(fileOut, cipher); 
 
         byte[] buffer = new byte[8];
-        int len = fileIn.read(buffer);
-        while (len != -1) 
+        int totalLen = 0;
+        for (int len = fileIn.read(buffer); len != -1; len = fileIn.read(buffer)) 
         {
-            cipherOut.write(buffer, 0, 1);
-            len = fileIn.read(buffer);
+            cipherOut.write(buffer, 0, len);
+            totalLen += len;
         }
         cipherOut.close();
         fileIn.close();
         fileOut.close();
+        System.out.printf("    %d bytes read %n", totalLen);
     }
     
+    private static void decrypt(Cipher cipher, Key key, String sourceFile, String destinationFile) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException
+    {
+        System.out.printf("decrypt %s to %s ...", sourceFile, destinationFile);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        FileInputStream fileIn = new FileInputStream(sourceFile);
+        FileOutputStream fileOut = new FileOutputStream(destinationFile);
+        CipherInputStream cipherIn = new CipherInputStream(fileIn, cipher); 
+
+        byte[] buffer = new byte[8];
+        int totalLen = 0;
+        for (int len = cipherIn.read(buffer); len != -1; len = cipherIn.read(buffer)) 
+        {
+            System.out.printf("read %d bytes %n", len);
+            fileOut.write(buffer, 0, len);
+            totalLen += len;
+        }
+        cipherIn.close();
+        fileIn.close();
+        fileOut.close();
+        System.out.printf(" %d bytes read %n", totalLen);
+    }
+
 }
