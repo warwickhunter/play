f229e93ab2dc1fc43dfbb449e85085a22a75eca2
Warwick Hunter <w.hunter@computer.org>
2011-08-12 19:51:38 +1000

    Code format correction. No other changes.

--- threadpool/src/threadpool/Threadpool.java
+++ threadpool/src/threadpool/Threadpool.java
@@ -11,65 +11,55 @@ import java.util.concurrent.TimeUnit;
  * An experiment with thread pools.
  * 
  * @author Warwick Hunter (w.hunter@computer.org)
- * @date   2010-09-12
+ * @date 2010-09-12
  */
-public class Threadpool 
-{
-    private static final int NUM_THREADS = 4;
+public class Threadpool {
+    
+    private static final int      NUM_THREADS = 4;
 
     private final ExecutorService m_pool;
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         new Threadpool().start();
     }
 
-    public Threadpool()
-    {
+    public Threadpool() {
         m_pool = Executors.newFixedThreadPool(NUM_THREADS);
     }
-    
-    public void start()
-    {
-        try
-        {
-            for (int i = 0; i < 50; ++i)
-            {
+
+    public void start() {
+        try {
+            for (int i = 0; i < 50; ++i) {
                 m_pool.execute(new Job(Integer.toString(i)));
             }
 
             System.out.println("shutdown");
             m_pool.shutdown();
-            
+
             System.out.println("waiting for all jobs to complete");
             m_pool.awaitTermination(30, TimeUnit.SECONDS);
             System.out.println("waiting complete");
         }
-        catch (InterruptedException e)
-        {
+        catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private static class Job implements Runnable
-    {
+    private static class Job implements Runnable {
+
         private final String m_id;
 
-        public Job(String id)
-        {
+        public Job(String id) {
             m_id = id;
         }
 
         @Override
-        public void run()
-        {
+        public void run() {
             System.out.printf("%s start on %s %n", m_id, Thread.currentThread().getName());
-            try
-            {
+            try {
                 Thread.sleep(500);
             }
-            catch (InterruptedException e)
-            {
+            catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.out.printf("%s end on %s %n", m_id, Thread.currentThread().getName());
