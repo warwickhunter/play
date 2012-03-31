99e0056a978b0f2631d2782e1ec9f67c01acc8c6
Warwick Hunter <w.hunter@computer.org>
2012-04-01 08:36:10 +1000

    Rework the GAE version of scrapemlc to be invoked by the GAE cron service. It then invokes the Cloudbees service.

--- ScrapeMlc.gae/src/org/computer/whunter/ScrapeMlcServlet.java
+++ ScrapeMlc.gae/src/org/computer/whunter/ScrapeMlcServlet.java
@@ -3,78 +3,45 @@
  */
 package org.computer.whunter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.meterware.httpunit.HttpUnitOptions;
-import com.meterware.httpunit.WebConversation;
-import com.meterware.httpunit.WebResponse;
 
-
+/**
+ * Invoke another web application and respond with the contents it returns.
+ * This service is invoked by a cron job using the GAE cron service and is
+ * run periodically. The Cloudbees system doesn't have a cron service, so
+ * I use this method to make use of the GAE cron service. 
+ * 
+ * @author Warwick Hunter (w.hunter@computer.org)
+ * @date   2012-04-01
+ */
 @SuppressWarnings("serial")
 public class ScrapeMlcServlet extends HttpServlet {
     
-    private static final String URL = "https://www.mlc.com.au/masterkeyWeb/execute/FramesetUnitPrices";
+    private static final String THE_URL = "http://scrapemlc.wasa.cloudbees.net/scrape";
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("MLC Unit Price");
-        String price = scrape();
-        resp.getWriter().println(price);
-        sendMail(price);
-    }
-
-    private String scrape() {
         try {
-            HttpUnitOptions.setScriptingEnabled(false);
-            WebConversation client = new WebConversation();
-            client.getResponse(URL);
-            WebResponse frame = client.getFrameContents("selection");
-
-            Pattern pattern = Pattern.compile("\"MLC Platinum Global Fund \\(closed\\),MasterKey Unit Trust,([^,]+),([^,]+),");
-            Matcher matcher = pattern.matcher(frame.getText());
-            if (matcher.find() && matcher.groupCount() > 1) {
-                return String.format("%s $%s %n", matcher.group(1), matcher.group(2));
+            URL url = new URL(THE_URL);
+            URLConnection con = url.openConnection();
+            resp.setContentType(con.getContentType());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                resp.getWriter().println(line);
             }
-        } catch (Exception e) {
-            return e.toString();
-        }
-        return null;
-    }
-    
-    private void sendMail(String msgBody) {
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
-
-        try {
-            MimeMessage msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress("fiona.m.hunter@gmail.com", "Fiona Hunter"));
-            msg.addRecipient(Message.RecipientType.TO, new InternetAddress("fiona.m.hunter@gmail.com", "Fiona Hunter"));
-            msg.addRecipient(Message.RecipientType.CC, new InternetAddress("warwickhunter@gmail.com", "Warwick Hunter"));
-            msg.setSubject("MLC");
-            msg.setText(msgBody);
-            Transport.send(msg);
-        } catch (AddressException e) {
+        } catch (MalformedURLException e) {
             log(e.toString());
-        } catch (MessagingException e) {
+        } catch (IOException e) {
             log(e.toString());
         }
-        catch (UnsupportedEncodingException e) {
-            log(e.toString());
-        }        
     }
 }
