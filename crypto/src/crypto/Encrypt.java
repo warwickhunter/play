d9859239b3cc9d73e300d2e70767a52d75c3dfdb
Warwick Hunter <w.hunter@computer.org>
2013-12-19 18:02:16 +1000

    Some updates to see if I can use or understand this code again.

--- crypto/src/crypto/Encrypt.java
+++ crypto/src/crypto/Encrypt.java
@@ -5,12 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.Key;
-import java.security.KeyStoreException;
+import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.Security;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -30,50 +28,45 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
  */
 public class Encrypt
 {
+    private static boolean USE_RANDOM_KEY = false;
+    private static boolean USE_KNOWN_KEY  = true;
+    
     private static final String KEYSTORE          = "crypto.ks";
     private static final String KEYSTORE_PASSWORD = "crypto";
     private static final String KEY_ALIAS         = "crypto";
     private static final String KEY_PASSWORD      = "crypto";
 
     /**
-     * @param args
-     *      element[0] is the file name to encrypt
-     * @throws KeyStoreException 
-     * @throws IOException 
-     * @throws CertificateException 
-     * @throws NoSuchAlgorithmException 
-     * @throws UnrecoverableKeyException 
-     * @throws BadPaddingException 
-     * @throws IllegalBlockSizeException 
-     * @throws NoSuchPaddingException 
-     * @throws InvalidKeyException 
+     * @param args args[0] is the file name to encrypt
      */
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
         	Security.addProvider(new BouncyCastleProvider());
-        	KeyGenerator keyGen = KeyGenerator.getInstance("DES", "BC");
-        	keyGen.init(new SecureRandom());
-        	Key key = keyGen.generateKey();
-        	
-//            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-//            FileInputStream keyStoreIn = new FileInputStream(KEYSTORE);
-//            keyStore.load(keyStoreIn, KEYSTORE_PASSWORD.toCharArray());
-//            keyStoreIn.close();
-            
-//            Key    key    = keyStore.getKey(KEY_ALIAS, KEY_PASSWORD.toCharArray());
-//            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding"); // RSA");
-            Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding", "BC");
 
-            for (int i = 0; i < args.length; ++i)
-            {
+        	Key key = null;
+            Cipher cipher = null;
+        	if (USE_RANDOM_KEY) {
+            	KeyGenerator keyGen = KeyGenerator.getInstance("DES", "BC");
+            	keyGen.init(new SecureRandom());
+            	key = keyGen.generateKey();
+            	cipher = Cipher.getInstance("DES/CBC/PKCS5Padding", "BC");
+        	}
+        	
+        	if (USE_KNOWN_KEY) {
+                KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                FileInputStream keyStoreIn = new FileInputStream(KEYSTORE);
+                keyStore.load(keyStoreIn, KEYSTORE_PASSWORD.toCharArray());
+                keyStoreIn.close();
+                key = keyStore.getKey(KEY_ALIAS, KEY_PASSWORD.toCharArray());
+                cipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
+        	}
+
+            for (int i = 0; i < args.length; ++i) {
                 encrypt(cipher, key, args[i], args[i] + ".crypt");
                 decrypt(cipher, key, args[i] + ".crypt", args[i] + ".decrypt");
             }
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -81,6 +74,7 @@ public class Encrypt
     private static void encrypt(Cipher cipher, Key key, String sourceFile, String destinationFile) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException
     {
         System.out.printf("encrypt %s to %s %n", sourceFile, destinationFile);
+        System.out.printf("key=%s%n", key.getEncoded());
         cipher.init(Cipher.ENCRYPT_MODE, key);
         FileInputStream fileIn = new FileInputStream(sourceFile);
         FileOutputStream fileOut = new FileOutputStream(destinationFile);
@@ -88,8 +82,7 @@ public class Encrypt
 
         byte[] buffer = new byte[16];
         int totalLen = 0;
-        for (int len = fileIn.read(buffer); len != -1; len = fileIn.read(buffer)) 
-        {
+        for (int len = fileIn.read(buffer); len != -1; len = fileIn.read(buffer)) {
             cipherOut.write(buffer, 0, len);
             totalLen += len;
         }
@@ -109,8 +102,7 @@ public class Encrypt
 
         byte[] buffer = new byte[8];
         int totalLen = 0;
-        for (int len = cipherIn.read(buffer); len != -1; len = cipherIn.read(buffer)) 
-        {
+        for (int len = cipherIn.read(buffer); len != -1; len = cipherIn.read(buffer)) {
             System.out.printf("read %d bytes %n", len);
             fileOut.write(buffer, 0, len);
             totalLen += len;
@@ -120,5 +112,4 @@ public class Encrypt
         fileOut.close();
         System.out.printf(" %d bytes read %n", totalLen);
     }
-
 }
