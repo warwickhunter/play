deaefaf74c47ec13ddaebb68d45e108021ab760f
Warwick Hunter <w.hunter@computer.org>
2017-11-24 09:53:13 +1000

    Experimenting with Java 9 modules.

---
+++ java9/modules/WelcomeApp/src/com.deitel.welcome/com/deitel/welcome/Welcome.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright Warwick Hunter 2017. All rights reserved.
+ */
+
+
+package com.deitel.welcome;
+
+/**
+ * Messing around with Java 9 packages. I am following the article in the Java Magazine September 2017.
+ * 
+ * @author Warwick Hunter (w.hunter@computer.org)
+ * @date   2017-11-24
+ */
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello from the world of Java 9 modules%n");
+    }
+
+}
---
+++ java9/modules/WelcomeApp/src/com.deitel.welcome/module-info.java
@@ -0,0 +1,6 @@
+// module-info.java
+// Module declaration for the com.deitel.welcome module
+
+module com.deitel.welcome {
+    requires java.base; 
+}
