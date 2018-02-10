f181452c4bbc27bf1b251f304632553871f8c71d
Warwick Hunter <whunter@anonyome.com>
2018-02-10 15:05:31 +1000

    Wrap up the chapters into a single main

---
+++ rxjavatest2/src/main/java/RxJava.java
@@ -0,0 +1,13 @@
+/**
+ * Experiments with RxJava from "Reactive Programming with RxJava"
+ *
+ * @author Warwick Hunter
+ * @since  2018-02-10
+ */
+public class RxJava {
+
+    public static void main(String[] args) throws InterruptedException {
+        RxJavaChapter2.main(args);
+        RxJavaChapter3.main(args);
+    }
+}
