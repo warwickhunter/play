c140ccf8f594f7ecf09cd1312172e84875bcac09
Warwick Hunter <w.hunter@computer.org>
2018-02-17 15:35:38 +1000

    Parallel lessons

--- rxjavatest2/src/main/java/RxJavaChapter4.java
+++ rxjavatest2/src/main/java/RxJavaChapter4.java
@@ -1,15 +1,16 @@
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.time.StopWatch;
 import org.apache.commons.lang3.tuple.Pair;
 
 import com.google.common.base.Stopwatch;
 
 import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Experiments with RxJava from "Reactive Programming with RxJava"
@@ -22,6 +23,7 @@ public class RxJavaChapter4 {
     public static void main(String[] args) throws InterruptedException {
         nonRxToRx();
         joinTwoObservables();
+        parallel();
     }
 
     private static void nonRxToRx() {
@@ -32,39 +34,56 @@ public class RxJavaChapter4 {
 
     /** Invoke some random web service to make things more realistic */
     private static List<String> nonRxMethod() throws IOException {
-        URL url = new URL("http://scooterlabs.com/echo?message=hello_world");
-        return IOUtils.readLines(url.openStream(), "UTF-8");
+        URL url = new URL("http://scooterlabs.com/echo?message=" + UUID.randomUUID().toString());
+        List<String> lines = IOUtils.readLines(url.openStream(), "UTF-8");
+        lines.add("thread=" + thread());
+        return lines;
     }
-    
+
     private static Observable<String> rxMethod() {
-        return Observable.defer(() -> 
+        return Observable.defer(() ->
                     Observable.fromIterable(nonRxMethod()));
+    }
+
+    private static String thread() {
+        return Thread.currentThread().getName();
     }
 
     private static void joinTwoObservables() {
         Stopwatch sw = Stopwatch.createStarted();
         fast()
-            .zipWith(slow(), Pair::of)
-            .subscribe(x -> System.out.printf("joinTwo %s%n", x));
+            .zipWith(slow(), Pair::of) // can be String::concat
+            .blockingSubscribe(x -> System.out.printf("joinTwo1 %s on %s %n", x, thread()));
         sw.elapsed(TimeUnit.MILLISECONDS);
-        System.out.printf("joinTwo took %s%n", sw);
+        System.out.printf("joinTwo1 took %s thread=%s %n", sw, thread());
     }
-    
+
     private static Observable<String> slow() {
-        try {
-            TimeUnit.MILLISECONDS.sleep(500);
-            return Observable.just("slow");
-        } catch (InterruptedException e) {
-            return Observable.error(e);
-        }
+        return Observable.just("slow thread=" + thread())
+                    .delay(1500, TimeUnit.MILLISECONDS);
     }
 
     private static Observable<String> fast() {
-        try {
-            TimeUnit.MILLISECONDS.sleep(50);
-            return Observable.just("fast");
-        } catch (InterruptedException e) {
-            return Observable.error(e);
-        }
+        return Observable.just("fast thread=" + thread())
+                    .delay(500, TimeUnit.MILLISECONDS);
+    }
+
+    private static void parallel() throws InterruptedException {
+        Observable.just(rxMethod())
+            .observeOn(Schedulers.io())
+            .flatMap(x -> x)
+            .mergeWith(rxMethod())
+            .mergeWith(rxMethod())
+            .filter(line -> line.contains("thread") || line.contains("utc"))
+            .blockingSubscribe(x -> System.out.printf("parallel1 %s on %s%n", x, thread()));
+
+        Observable.just(1)
+            .observeOn(Schedulers.io())
+            .flatMap(x -> rxMethod())
+            .mergeWith(rxMethod())
+            .mergeWith(rxMethod())
+            .mergeWith(rxMethod())
+            .filter(line -> line.contains("thread") || line.contains("utc"))
+            .blockingSubscribe(x -> System.out.printf("parallel2 %s on %s%n", x, thread()));
     }
 }
