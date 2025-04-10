fc76da5f0e5166c86bd221b3927f5e52ca183387
Warwick Hunter <w.hunter@computer.org>
2025-04-10 16:20:34 +1000

    guards

--- typescript_course/tsclasses/src/guards.ts
+++ typescript_course/tsclasses/src/guards.ts
@@ -36,8 +36,12 @@ const dbSource2: DBSource2 = {
 
 type Source2 = FileSource2 | DBSource2;
 
+function isFile(source: Source2) {
+    return source.type === 'file';
+}
+
 function loadData2(source: Source2) {
-    if (source.type == 'file') {
+    if (isFile(source)) {
         console.log(`Using ${source.path}`);
         return
     }
@@ -46,3 +50,32 @@ function loadData2(source: Source2) {
 
 loadData2(fileSource2);
 loadData2(dbSource2);
+
+class User7 {
+    constructor(public name: string) {}
+
+    join() {
+        // ...
+    }
+}
+
+class Admin {
+    constructor(permissions: string[]) {}
+
+    scan() {
+        // ...
+    }
+}
+
+const user7 = new User7('Max');
+const admin = new Admin(['ban', 'restore']);
+
+type Entity = User7 | Admin;
+
+function init(entity: Entity) {
+    if (entity instanceof User7) {
+        entity.join();
+        return;
+    }
+    entity.scan();
+}
